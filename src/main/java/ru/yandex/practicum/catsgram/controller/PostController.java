445825a38e6699849860211d832f6acae9255e95
sasha1Gury <sashagury456123@mail.ru>
2024-01-29 17:22:31 +0300

    feat: b.i.g.

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
@@ -28,5 +25,10 @@ public class PostController {
     @PostMapping(value = "/post")
     public Post create(@RequestBody Post post) {
         return postService.create(post);
+    }
+
+    @GetMapping("/posts/{id}")
+    public Post getPost(@PathVariable Integer id) {
+        return postService.findPostById(id);
     }
 }
--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,21 +1,52 @@
 package ru.yandex.practicum.catsgram.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.PostNotFoundException;
+import ru.yandex.practicum.catsgram.exception.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PostService {
+    private final UserService userService;
     private final List<Post> posts = new ArrayList<>();
+
+    private static Integer globalId = 0;
+
+    @Autowired
+    public PostService(UserService userService) {
+        this.userService = userService;
+    }
 
     public List<Post> findAll() {
         return posts;
     }
 
+    private static Integer getNextId(){
+        return globalId++;
+    }
+
     public Post create(Post post) {
+        User postAuthor = userService.findUserByEmail(post.getAuthor());
+        if (postAuthor == null) {
+            throw new UserNotFoundException(String.format(
+                    "Пользователь %s не найден",
+                    post.getAuthor()));
+        }
+
+        post.setId(getNextId());
         posts.add(post);
         return post;
+    }
+
+    public Post findPostById(Integer postId) {
+        return posts.stream()
+                .filter(p -> p.getId().equals(postId))
+                .findFirst()
+                .orElseThrow(() -> new PostNotFoundException(String.format("Пост № %d не найден", postId)));
     }
 }
