5964b3f1074f72d4cf9d26406ee073f95316169b
sasha1Gury <sashagury456123@mail.ru>
2024-02-04 16:36:22 +0300

    refactor: ErrorResponse now record

--- src/main/java/ru/yandex/practicum/catsgram/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/ErrorResponse.java
@@ -3,10 +3,5 @@ package ru.yandex.practicum.catsgram.model;
 import lombok.Getter;
 
 @Getter
-public class ErrorResponse {
-    private final String error;
-
-    public ErrorResponse(String error) {
-        this.error = error;
-    }
+public record ErrorResponse(String error) {
 }
