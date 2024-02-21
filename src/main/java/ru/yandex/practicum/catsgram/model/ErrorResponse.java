a96f259d814551d7c33810a93c966b9f3ab195d5
sasha1Gury <sashagury456123@mail.ru>
2024-02-21 19:54:07 +0300

    fix: lombok fix

--- src/main/java/ru/yandex/practicum/catsgram/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/ErrorResponse.java
@@ -3,5 +3,11 @@ package ru.yandex.practicum.catsgram.model;
 import lombok.Getter;
 
 @Getter
-public record ErrorResponse(String error) {
+public class ErrorResponse {
+    private final String error;
+
+    public ErrorResponse(String error) {
+        this.error = error;
+    }
 }
+
