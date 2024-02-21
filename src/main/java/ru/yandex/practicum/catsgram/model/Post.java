c8b0f055a747036c37e97aed5c917a6f09968b89
sasha1Gury <sashagury456123@mail.ru>
2024-02-21 19:54:37 +0300

    refactor: now fields like in db

--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -1,14 +1,17 @@
 package ru.yandex.practicum.catsgram.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.Instant;
+import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class Post {
     private Integer id;
-    private final String author; // автор
-    private final Instant creationDate = Instant.now(); // дата создания
+    private final User author; // автор
+    private final LocalDate creationDate; // дата создания
     private String description; // описание
     private String photoUrl; // url-адрес фотографии
 }
--- src/main/java/ru/yandex/practicum/catsgram/model/User.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -1,12 +1,16 @@
 package ru.yandex.practicum.catsgram.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-    private String email;
+    private String id;
     private String nickname;
-    private LocalDate birthdate;
+    private String username;
 }
